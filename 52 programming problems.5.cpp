8843d6bc7fa8fbe7b6b369447e123fb3edeaeccc
Shariful Islam Prince <54590868+001sharifulIslamnstu@users.noreply.github.com>
2020-02-19 23:52:11 -0800

    Add files via upload

---
+++ 52 programming problems.5.cpp
@@ -0,0 +1,24 @@
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+vector<int> g;
+int main()
+{
+  int t,n;
+  cin>>t;
+  while(t--)
+  {
+      cin>>n;
+      for(int i=1;i<=n;i++)
+      {
+        for(int j=1;j<=n;j++)
+      {
+          cout<<"*";
+      }
+      cout<<"\n";
+      }
+      cout<<"\n";
+  }
+}
